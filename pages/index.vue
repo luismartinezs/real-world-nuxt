9ed13babb6c9656b0a8d3adfa2b440e1fe6a0033
luismartinezs <martinez.s.luis@gmail.com>
2021-04-28 17:38:05 +0200

    Fetch data with axios, server error handling

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
     <h1>Events</h1>
+    <EventCard v-for="(event, index) in events" :key="index" :event="event" />
   </div>
 </template>
 
 <script>
 export default {
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:3000/events')
+      .then((res) => {
+        return {
+          events: res.data,
+        }
+      })
+      .catch(() => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time, please try later',
+        })
+      })
+  },
   head() {
     return {
       title: 'Event listing',
